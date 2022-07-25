765cecde0ef980b6b12292abee094da41bec0b0c
Olga <andrijankina@gmail.com>
2022-07-25 13:35:55 +0300

    Initial commit

---
+++ src/main/java/org/example/Kvadratas.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Kvadratas extends Figura {
+    public Kvadratas(int krastine1) {
+        super(krastine1, krastine1);
+    }
+}
---
+++ src/main/java/org/example/Trikampis.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Trikampis extends Figura {
+    //@Override
+    //public void skaiciuokPlota() {
+        //super.skaiciuokPlota();
+        //getPlotas =(krastine1*krastine2)/2;
+    //}
+
+    public Trikampis(int krastine1, int krastine2) {
+        super(krastine1, krastine2);
+    }
+
+
+        }
+
+
